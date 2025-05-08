3556d72b1cd6b3ffa8d05d19da424de6c6c12c28
Emanuela <emanuelapresti92@gmail.com>
2025-05-08 15:53:34 +0200

    added switch-case

---
+++ switch-case/index.js
@@ -0,0 +1,16 @@
+let score = parseInt(prompt("Inserisci il tuo voto"));
+
+switch (score) {
+    case 1: 
+        console.log("voto ottimo");
+        break;
+    case 2:
+        console.log("voto buono");
+        break;
+    case 3:
+        console.log("voto sufficiente");
+        break;
+    case 4:
+        console.log("voto insufficiente");
+        break;
+}
