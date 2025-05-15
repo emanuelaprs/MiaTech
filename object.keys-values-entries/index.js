3f3720a982f809b12cd40a5741f995cf5402b217
Emanuela <emanuelapresti92@gmail.com>
2025-05-15 17:49:30 +0200

    added object.entries

---
+++ object.keys-values-entries/index.js
@@ -0,0 +1,22 @@
+const student = {
+    name: "Pietro",
+    age: 16,
+    grade: "2A",
+    school: "Liceo Classico"
+}
+
+const key = Object.keys(student);
+
+console.log(key);
+
+// Esercizio successivo Object.values() - stesso oggetto
+
+const value = Object.values(student);
+
+console.log(value);
+
+// Esercizio successivo Object.entries() - stesso oggetto
+
+const entry = Object.entries(student);
+
+console.log(entry);
