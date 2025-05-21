e9c7fb3946dc396a35a65ba1d24129ca13d782b8
Emanuela <emanuelapresti92@gmail.com>
2025-05-21 19:41:05 +0100

    modified destructuring

--- destructuring/index.js
+++ destructuring/index.js
@@ -1,6 +1,15 @@
+// Esercizio Estrazione elementi base
+
 let numebrs = [1, 2, 3, 4, 5];
 
 let [primo, secondo] = numebrs;
 
 console.log(primo);
 console.log(secondo);
+
+// Esercizio Destructuring con nomi di variabili diversi
+
+let [primoNumero, secondoNumero] = numebrs;
+
+console.log(primoNumero);
+console.log(secondoNumero);
