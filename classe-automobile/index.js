1140363cff5f6b89289af742ead1802d708287d7
Emanuela <emanuelapresti92@gmail.com>
2025-05-22 10:32:49 +0100

    added classe-automobile

---
+++ classe-automobile/index.js
@@ -0,0 +1,33 @@
+// Esercizio Automobile
+
+class automobile {
+     constructor (marca, modello, anno, chilometraggio = 0) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = chilometraggio;
+    }
+
+    descrizione() {
+        return `Automobile: ${this.marca} ${this.modello}, anno ${this.anno}`;
+  }
+    aggiungiChilometri(km) {
+        this.chilometraggio += km;
+    }
+
+    mostraChilometraggio() {
+        return this.chilometraggio;
+    }
+}
+
+const car = new automobile("Opel", "Corsa", 2010);
+console.log(car.descrizione()); 
+
+// Esercizio Aggiungiamo metodi e proprietà -- (aggiunto allo stesso codice sopra)
+
+car.aggiungiChilometri(100);
+console.log(car.mostraChilometraggio());
+
+car.aggiungiChilometri(80);
+console.log(car.mostraChilometraggio());
+
