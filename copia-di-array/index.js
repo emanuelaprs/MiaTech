7d089ed22d3e6855a9a192f34f0a350681d11678
Emanuela <emanuelapresti92@gmail.com>
2025-05-28 19:03:02 +0200

    modified copia-di-array

--- copia-di-array/index.js
+++ copia-di-array/index.js
@@ -1,3 +1,5 @@
+// Copia di Array
+
 const numbers = [1, 2, 3, 4, 5];
 
 const copiaNumbers = [...numbers];
@@ -7,3 +9,12 @@ numbers.push(6);
 console.log(numbers);
 
 console.log(copiaNumbers);
+
+// Unire due array
+
+const numeri1 = [1, 2, 3, 4];
+const numeri2 = [5, 6, 7, 8];
+
+const unioneNumeri = [...numeri1, ...numeri2];
+
+console.log(unioneNumeri);
