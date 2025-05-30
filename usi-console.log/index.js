e929e5682873847edcc64a76d7c353cc88a51810
Emanuela <emanuelapresti92@gmail.com>
2025-05-30 11:05:58 +0200

    modified usi-console.log again

--- usi-console.log/index.js
+++ usi-console.log/index.js
@@ -29,3 +29,24 @@ if (controllo === "Errore") {
 }
 
 // Ho provato variare let controllo per visionare le varie possibilità ( tutto ok!)
+
+
+// Esercizio Utilizzare console.table e console.group
+
+const utenti = [
+    { nome: "Paola", cognome: "Rossi", età: 68 },
+    { nome: "Ignazio", cognome: "Bianchi", età: 69 },
+    { nome: "Alberto", cognome: "Verdi", età: 58 }
+];
+
+console.table(utenti);
+
+console.group("Dettagli utente 1");
+console.log(utenti[0]);
+console.group("Messaggio");
+console.log("Questo è un gruppo di cugini");
+console.groupEnd();
+console.log("Fine script");
+console.groupEnd();
+
+
