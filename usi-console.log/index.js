a8be30ac4e4c9ee47f7ec41fbae0a254b1c2332f
Emanuela <emanuelapresti92@gmail.com>
2025-05-30 09:27:00 +0200

    usi-console.log

---
+++ usi-console.log/index.js
@@ -0,0 +1,21 @@
+// Esercizio Utilizzare console.log
+
+let numero = 17;
+
+let testo = "Buon Compleanno!";
+
+let appuntamento = ["lunedì", "martedì", "mercoledì", "giovedì", "venerdì"];
+
+let persona = {
+    nome: "Luna",
+    età: 10
+};
+
+console.log(numero);
+console.log(testo);
+console.log(appuntamento);
+console.log(persona);
+
+
+
+
