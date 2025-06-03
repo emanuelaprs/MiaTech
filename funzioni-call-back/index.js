62d00532e7b441508a4dbcfcaa93f2bafd983937
Emanuela <emanuelapresti92@gmail.com>
2025-06-03 09:57:57 +0200

    modified funzioni-call-back

--- funzioni-call-back/index.js
+++ funzioni-call-back/index.js
@@ -1,3 +1,5 @@
+// Esercizio Funzione di base con callback
+
 function conCallback(callback) {
     const operazione = 2 + 2;
     console.log("Risultato somma:", operazione);
@@ -8,3 +10,16 @@ function conCallback(callback) {
  }
 
  conCallback(chiamoCallback);
+
+ // Esercizio Funzione con callback e passaggio di parametri
+
+ function eseguiOperazione(valore, callback) {
+    const risultato = valore * 2;
+    callback(risultato);
+ }
+
+ function mostraRisultato(risultato) {
+    console.log("Ecco il risultato:", risultato);
+ }
+ 
+ eseguiOperazione(5, mostraRisultato);
