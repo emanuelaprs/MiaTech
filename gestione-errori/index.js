30cfad796bf4a4eaea129239b05e3bc598127618
Emanuela <emanuelapresti92@gmail.com>
2025-06-04 18:42:33 +0200

    modified gestione-errori

--- gestione-errori/index.js
+++ gestione-errori/index.js
@@ -10,3 +10,23 @@ promessaRifiutata()
 .catch(error => {
     console.log(error.message);
 })
+
+// Esercizio Gestione degli errori con then e catch
+
+function promessaBool(boolValue) {
+    return new Promise((resolve, reject) => {
+        if (boolValue) {
+            resolve ('Promessa risolta!');
+        } else {
+            reject('Promessa rifiutata: errore!');
+        }
+    });
+}
+
+ promessaBool(true)
+ .then(result => console.log('Successo:', result))
+ .catch(error => console.log('Errore:', error));
+
+ promessaBool(false)
+ .then(result => console.log('Successo:', result))
+  .catch(error => console.log('Errore:', error));
