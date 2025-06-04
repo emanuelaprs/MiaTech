c036ff0d03544024a36d0a75dc7af6ba5ff3cfc8
Emanuela <emanuelapresti92@gmail.com>
2025-06-04 16:11:55 +0200

    modified promesse

--- promesse/index.js
+++ promesse/index.js
@@ -11,3 +11,26 @@ function promessa() {
 promessa().then((messaggio) => {
     console.log(messaggio);
 })
+
+// Esercizio Gestione di una promessa con catch
+
+function promessa1(condizione) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (condizione) {
+                resolve("La promessa si è risolta dopo 2 secondi");
+            } else {
+                reject("Errore: promessa riufiutata")
+            }
+        }, 2000);
+    });
+}
+
+promessa1(false)
+    .then((messaggio) => {
+    console.log(messaggio);
+})
+    .catch((errore) => {
+    console.error(errore);
+});
+
