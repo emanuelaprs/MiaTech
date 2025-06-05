0eac602481816b7b0c5afebade1981842fcd2a75
Emanuela <emanuelapresti92@gmail.com>
2025-06-05 22:19:56 +0200

    modified promise.type

--- promise.type/index.js
+++ promise.type/index.js
@@ -22,3 +22,29 @@ Promise.all([promessaVeloce(), promessaLenta()])
     console.log(risultati[0]);
     console.log(risultati[1]);
 })
+
+// Esercizio Utilizzare Promise.race
+
+function promessaSvelta() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Promessa svelta completata');
+        }, 1000);
+    });
+}
+
+function promessaCalma() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Promessa calma completata');
+        }, 2000);
+    });
+}
+
+Promise.race([promessaSvelta(), promessaCalma()])
+.then((risultato) => {
+    console.log(risultato);
+})
+.catch((errore) => {
+    console.error(errore);
+});
