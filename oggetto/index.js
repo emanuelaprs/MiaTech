8c5b2bbadb94207c5055d3daf191b9a2005f641b
Emanuela <emanuelapresti92@gmail.com>
2025-07-12 17:14:49 +0200

    added exercises

---
+++ oggetto/index.js
@@ -0,0 +1,23 @@
+// Esercizio Crea oggetto
+
+let person = {
+    name: "Roberto",
+    age: 45,
+    city: "Napoli",
+};
+
+console.log(person);
+
+// Esercizio Accedi ad una proprietà
+
+console.log(person.age);
+
+// Esercizio Modifica proprietà
+
+person.age = 26;
+
+// Esercizio Aggiungi una nuova proprietà
+
+person.job = "Scrittore";
+
+console.log(person.job);
