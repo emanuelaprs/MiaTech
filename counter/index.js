202e9b016bb55aa1d3cb98ff52ddc94c8a873891
Emanuela <emanuelapresti92@gmail.com>
2025-07-12 18:11:13 +0200

    modified counter

---
+++ counter/index.js
@@ -0,0 +1,14 @@
+function createCounter() {
+    let count = 0;
+    return {
+        increment: function() {
+            count += 1;
+            return count;
+        },
+        decrement: function() {
+            count -= 1;
+            return count;
+        }
+    };
+}
+
