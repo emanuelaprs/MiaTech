a7ed2ecad3d7a947e958d0fe90d267086c8c79f7
Emanuela <emanuelapresti92@gmail.com>
2025-12-12 19:01:10 +0100

    Cancellati manualmente tutti i file per partire da zero

--- cambio valore/index.js
+++
@@ -1,7 +0,0 @@
-let numero = 5;
-
-console.log(numero);
-
-numero = 10;
-
-console.log(numero);
--- if-else/index.js
+++
@@ -1,6 +0,0 @@
-let number = 3; 
-    if (number > 5) {
-        console.log("la variabile è maggiore di 5");
-    } else {
-        console.log("la variabile è minore di 5");
-    }
